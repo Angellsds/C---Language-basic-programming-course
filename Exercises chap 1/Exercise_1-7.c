fa951102df27210c5217cb5ea2fb46d5f4562944
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-02-20 21:45:41 -0600

    Add files via upload

--- Exercises chap 1/Exercise_1-7.c
+++ Exercises chap 1/Exercise_1-7.c
@@ -1,23 +1,11 @@
 #include<stdio.h>
 
 /* count characters in input; 1st version */
-void main(){
+int main(){
+        int nc;
 
-        int c, e1, t1, b1 ;
-
-        e1 = 0;
-        t1 = 0;
-        b1 = 0;
-        while ((c = getchar()) != EOF){
-            if(c == '\n'){
-            ++ e1;
-            }
-            if(c == '\t'){
-            ++ t1;
-            }
-            if(c == ' '){
-            ++ b1;
-            }
-        }
-        printf("Blanks: %d\nTabs: %d\nNew Lines: %d", b1, t1, e1);
+        nc = 0;
+        while (getchar() != EOF)
+            ++nc;
+        printf("%1d\n", nc);
 }
