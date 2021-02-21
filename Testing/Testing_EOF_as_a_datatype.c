737f14a8ca821e88eb3c4a3189702691d9405a57
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-02-20 18:06:12 -0600

    Add files via upload

---
+++ Testing/Testing_EOF_as_a_datatype.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+/*
+    int x = EOF;
+    printf("The value of EOF = %d\n", x);\
+*/
+
+/*
+    int c;
+    while ( c = (getchar() != EOF) ) {
+        printf("%d\n", c);
+            }
+        printf("%d\n", c);
+
+*/
+//    printf("%c", EOF); // is not printing
+//    printf("%d", EOF);  // this does
+
+        printf("The value of EOF = %d\n", EOF);
+        printf("The value of EOF = %d\n", EOF + 1); //we can do math operations with it
+}
