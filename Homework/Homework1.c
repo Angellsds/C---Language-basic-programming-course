7067f91e941b3ee9d503cc1f98c3e2544c50b8b1
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-02-25 14:34:11 -0600

    Update Homework1.c

--- Homework/Homework1.c
+++ Homework/Homework1.c
@@ -9,10 +9,10 @@ int main(){
     {
         if(c == '<')
         {
-            while((c = getchar()) == stro[i]){
+            while((c = getchar()) == stro[i]){		//check if the word between <> is strong
                 i++;
             }
-            if(c == scrip[1]){ //c
+            if(c == scrip[1]){ //c			//this chain of if check if the word between is script in case the word isn't strong
                 c = getchar();
                 if(c == scrip[2]){  //r
                     c = getchar();
@@ -21,13 +21,13 @@ int main(){
                         if(c == scrip[4]){  //p
                             c = getchar();
                             if(c == scrip[5]){  //t
-                        while ((c = getchar()) != '>'){
+                        while ((c = getchar()) != '>'){		//getchar with while to find the end of the character 
                             ;
                         }
-                        while((c = getchar()) != '/'){
+                        while((c = getchar()) != '/'){		//to get this character to ensure that we reach the end of the tag
                             ;
                         }
-                        while((c = getchar()) != '>'){
+                        while((c = getchar()) != '>'){		//to ensure that we are in the end of tne tag
                             ;
                             }
                         }
@@ -35,23 +35,23 @@ int main(){
                   }
                }
             }
-            if(i == 13){
-                i =0;
+            if(i == 13){	// if i =  13 i is going to reset its value, bc i is the number that tells to my program when the tag strong finish
+                i =0;		// restart the i value
             }
-             while(c != EOF && c != '>'){
+             while(c != EOF && c != '>'){	// to get all character between <>
                 c = getchar();
             }
         }
-        else if (i == 6 && i < 13 && c != '<'){
+        else if (i == 6 && i < 13 && c != '<'){		// if c != '/' this else if happen, and replace each blank with undersocre
                 if(c == ' '){
                     printf("_");
 
                 }
                 putchar(c);
            }
-            if ((i != 6 && c != '<' && c != '>') || c == '\n'){
+            if ((i != 6 && c != '<' && c != '>') || c == '\n'){	  //to put words between tags <> ** <>
             putchar(c);
-            i = 0;
+            i = 0;		//restart the count of i bc i wasn't equal to 6 and that means the tag wasn't strong
             }
     }
 }
