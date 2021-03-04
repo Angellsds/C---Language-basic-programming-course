d9c9d01466344695860cc5e4b7a497d1881fc4fe
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-03-03 20:20:16 -0600

    Update Homework2.c

--- Homework/Homework2.c
+++ Homework/Homework2.c
@@ -1,14 +1,16 @@
 
 #include <stdio.h>
-#define k 30    //cantidad de numeros primos a buscar    ~SOLO CAMBIAR ESTE VALOR PARA TAREA~
-#define MAX 128 // para 8 bits
+#define k  10   //cantidad de numeros primos a buscar    ~SOLO CAMBIAR ESTE VALOR PARA TAREA~
+#define MAX 128 // para 8 bits = 128   16 bits = 32768    32 bits = 2147483648  ~EN CASO DE ASIGNAR UN VALOR K QUE DE UN VALOR QUE NO PUEDE SER REPRESENTADO POR 8 BITS~
+int prime(/**/);
+int contador (/**/);
 
-int main (){   //consigue los primeros k numeros primos
+long main (){   //consigue los primeros k numeros primos
 
-	int x, i,cont = 0;
-	int state = 0;
-    unsigned int binario = MAX;
-    printf(k <= 1 ? "El primer numero primo es:\n\n" :"Los primeros %d numeros primos son:\n\n", k;
+	long x, i,cont = 0;
+	long state = 0;
+    unsigned long binario = MAX;
+    printf(k <= 1 ? "El primer numero primo es:\n\n" :"Los primeros %d numeros primos son:\n\n", k);
 	for(i = 2 ; cont < k ;i++){
         state = 0;
 		for(x = 2 ; x <= i ;x++){
@@ -33,5 +35,5 @@ int main (){   //consigue los primeros k numeros primos
       printf("\n");
     }
 
-	}
+  }
 }
