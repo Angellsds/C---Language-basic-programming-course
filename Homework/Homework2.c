b4523be60420f667e7eb47d914144cde40cf9460
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-03-03 21:39:11 -0600

    Update Homework2.c

--- Homework/Homework2.c
+++ Homework/Homework2.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #define k  10   //cantidad de numeros primos a buscar    ~SOLO CAMBIAR ESTE VALOR PARA TAREA~
 #define MAX 128 // para 8 bits = 128   16 bits = 32768    32 bits = 2147483648  ~EN CASO DE ASIGNAR UN VALOR K QUE DE UN VALOR QUE NO PUEDE SER REPRESENTADO POR 8 BITS~
-int prime(/**/);
-int contador (/**/);
 
-long main (){   //consigue los primeros k numeros primos
+int main (){   //consigue los primeros k numeros primos
 
 	long x, i,cont = 0;
 	long state = 0;
