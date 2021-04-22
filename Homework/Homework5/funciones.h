ec6082b65493594ff5f0f8fcc147e67fc7764c1c
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-04-22 01:12:41 -0500

    Update funciones.h

--- Homework/Homework5/funciones.h
+++ Homework/Homework5/funciones.h
@@ -4,16 +4,16 @@
 #define FLUSH fflush(stdin)
 
 void game_mode(void);
+void custom(void);
+void error(void);
 void default_minefield(void);
 void minefield_generator(void);
 void print_minefield(void);
+void print_final_minefield(void);
 void answer(void);
 void mine(void);
-void print_final_minefield(void);
 void win(void);
-void play_again(void);
 void game_over(void);
-void custom(void);
-void error(void);
+void play_again(void);
 
 #endif // FUNCIONES_H_INCLUDED
