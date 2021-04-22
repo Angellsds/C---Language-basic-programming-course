2fccebb24ba1942ec6f0fd2a31897237a5e5db14
Angellsds <78930815+Angellsds@users.noreply.github.com>
2021-04-22 01:20:40 -0500

    Add files via upload

---
+++ Homework/Homework5/Opcion2/Opcion2_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "funciones.h"
+
+
+int main()
+{
+    printf("\t\t\t\tWelcome to Minesweeper\n");
+    game_mode();
+    return 0;
+}
+
+
